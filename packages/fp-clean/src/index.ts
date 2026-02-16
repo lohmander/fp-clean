ef44b740f08c1ba936fb8dd9cda56ae909860fd7
Hannes Lohmander <hannes@lohmander.org>
2026-02-16 17:37:50 +0100

    fix: Fix: Rename exports to use namespaces
    Remove unused types file

--- packages/fp-clean/src/index.ts
+++ packages/fp-clean/src/index.ts
@@ -1,3 +1,4 @@
-export * from "./Operation";
-export * from "./Env";
+export * as Operation from "./Operation";
+export * as Env from "./Env";
+export * as Result from "./Env";
 export { pipe } from "./pipe";
--- packages/fp-clean/src/types.ts
+++
@@ -1,7 +0,0 @@
-export type Awaitable<T> = T | Promise<T>;
-
-export type UnionToIntersection<U> = (
-  U extends any ? (x: U) => void : never
-) extends (x: infer I) => void
-  ? I
-  : never;
